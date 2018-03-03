ef063c09a7d0847df3ef28444f4a7cfbe7249f23
Kyle Van Bergen <kyleavb@gmail.com>
2018-03-02 16:38:59 -0800

    re-added file

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,11 @@
+module.exports = function(req, res, next){
+  if(!req.user){
+    console.log("Attempted to access disallowed site");
+    console.log("Redirecting to root")
+    req.flash("error", "Please Log in to access this portions of the site! LOSER")
+    res.redirect('/login');
+  }else{
+    next();
+  }
+
+};
